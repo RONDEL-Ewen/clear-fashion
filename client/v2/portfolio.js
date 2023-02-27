849dd3fff2e94df42dcc00c017c4ff7d3321c979
RONDEL-Ewen <ewen.rondel@edu.devinci.fr>
2023-02-27 13:44:05 +0100

    Step 2 finished

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -4,31 +4,35 @@
 /*
 Description of the available api
 GET https://clear-fashion-api.vercel.app/
-
 Search for specific products
-
 This endpoint accepts the following optional query string parameters:
-
 - `page` - page of products to return
 - `size` - number of products to return
-
 GET https://clear-fashion-api.vercel.app/brands
-
 Search for available brands list
 */
 
 // current products on the page
 let currentProducts = [];
 let currentPagination = {};
-let currentBrand = "All";
+let brandsCount = 0;
+let recentProducts = 0;
+let lastRelease = NaN;
+let p50 = 0;
+let p90 = 0;
+let p95 = 0;
 
 // instantiate the selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
-const selectBrand = document.querySelector('#brand-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
-const selectSortingType = document.querySelector('#sort-select');
+const brandSelect = document.querySelector('#brand-select');
+const sortSelect = document.querySelector('#sort-select');
+const showOnlySelectSale = document.querySelector('#showOnly-select-sale');
+const showOnlySelectNew = document.querySelector('#showOnly-select-new');
+const showOnlySelectFavorite = document.querySelector('#showOnly-select-favorite');
+const productDiv = document.querySelectorAll(".product");
 
 /**
  * Set global value
@@ -40,71 +44,89 @@ const setCurrentProducts = ({result, meta}) => {
   currentPagination = meta;
 };
 
-const sortProducts = ({sortingType}) => {
-  const sortedProducts = [];
-  if(sortingType == 'price-asc') {
-    console.log('1');
-    sortedProducts = currentProducts.sort(function compare(a,b) {
-      if(a.price < b.price) return -1;
-      if(a.price > b.price) return 1;
-      return 0;
-    });
-  }
-  if(sortingType == 'price-desc') {
-    console.log('2');
-    sortedProducts = currentProducts.sort(function compare(a,b) {
-      if(a.price < b.price) return 1;
-      if(a.price > b.price) return -1;
-      return 0;
-    });
-  }
-  if(sortingType == 'date-asc') {
-    console.log('3');
-    sortedProducts = currentProducts.sort(function compare(a,b) {
-      if(a.released < b.released) return 1;
-      if(a.released > b.released) return -1;
-      return 0;
-    });
-  }
-  if(sortingType == 'date-desc') {
-    console.log('4');
-    sortedProducts = currentProducts.sort(function compare(a,b) {
-      if(a.released < b.released) return -1;
-      if(a.released > b.released) return 1;
-      return 0;
-    });
-  }
-  setCurrentProducts({sortedProducts, currentPagination});
-}
-
-/**
- * Set global value
- * @param {String} brandSelection
- */
-const setCurrentBrand = ({brandSelection}) => {
-  currentBrand = brandSelection;
-}
-
 /**
  * Fetch products from api
  * @param  {Number}  [page=1] - current page to fetch
  * @param  {Number}  [size=12] - size of the page
  * @return {Object}
  */
-const fetchProducts = async (page = 1, size = 12, brand = currentBrand) => {
+const fetchProducts = async (page = 1, size = 12, brand = "all", sortBy = "price-asc", filter = [false, false, false]) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}` + (brand!=="All" ? `&brand=${brand}` : "")
+      `https://clear-fashion-api.vercel.app?size=999` + (brand !== "all" ? `&brand=${brand}` : "")
     );
-
     const body = await response.json();
 
     if (body.success !== true) {
       console.error(body);
       return {currentProducts, currentPagination};
     }
+    var result = body.data.result;
+    
+    // filters
+    if(filter[0]) {
+      result = result.filter(product => product.price < 50);
+    }
+    if(filter[1]) {
+      result = result.filter(product => (new Date() - new Date(product.released)) / (1000 * 60 * 60 * 24) < 14);
+    }
+    if(filter[2]) {
+      result = result.filter(product => (JSON.parse(localStorage.getItem("favorites")) || []).includes(product.uuid));
+    };
 
-    return body.data;
+    var meta = {
+      currentPage: page,
+      pageCount: Math.ceil(result.length / size),
+      pageSize: size,
+      count: result.length
+    };
+    
+    if(sortBy === "price-asc") {
+      result.sort((a, b) => a.price - b.price);
+    }
+    else if(sortBy === "price-desc") {
+      result.sort((a, b) => b.price - a.price);
+    }
+    else if(sortBy === "date-asc") {
+      result.sort((a, b) => new Date(b.released) - new Date(a.released));
+    }
+    else if(sortBy === "date-desc") {
+      result.sort((a, b) => new Date(a.released) - new Date(b.released));
+    }
+
+    brandsCount = 0
+    if(result.length > 0){
+      result.reduce((acc, product) => {
+        if(!acc[product.brand]) {
+          acc[product.brand] = 1;
+          brandsCount++;
+        }
+        return acc;
+      }, {});
+    };
+
+    recentProducts = result.filter(product => (new Date() - new Date(product.released)) / (1000 * 60 * 60 * 24) < 14).length;
+
+    lastRelease = result.length > 0 ? result.reduce(function(a,b) {
+      return new Date(a.released) > new Date(b.released) ? a : b;
+    }).released : "Nan";
+
+    if(result.length > 0)
+    {
+      p50 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length / 2)].price;
+      p90 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length * 0.9)].price;
+      p95 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length * 0.95)].price;
+    }
+    else
+    {
+      p50 = 0;
+      p90 = 0;
+      p95 = 0;
+    }
+
+    var result = result.slice((page - 1) * size, page * size);
+    return {result, meta};
+    
   } catch (error) {
     console.error(error);
     return {currentProducts, currentPagination};
@@ -118,14 +140,17 @@ const fetchProducts = async (page = 1, size = 12, brand = currentBrand) => {
 const renderProducts = products => {
   const fragment = document.createDocumentFragment();
   const div = document.createElement('div');
+  let i = -1;
   const template = products
     .map(product => {
+      i++;
       return `
       <div class="product" id=${product.uuid}>
         <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
-        <span>${product.price}</span>
-      </div>
+        <a href="${product.link}" target="_blank">${product.name}</a>
+        <span>${product.price}€ </span><span id="${product.uuid}-fav">`
+      + ((JSON.parse(localStorage.getItem("favorites")) || []).includes(product.uuid) ? `🖤 <button onclick=deleteToFavorite("` + product.uuid + `")>Delete from favorite</button>` : `<button onclick=addToFavorite(currentProducts[${i}].uuid)>Favorite</button>`) + `
+      </span></div>
     `;
     })
     .join('');
@@ -161,47 +186,137 @@ const renderIndicators = pagination => {
   spanNbProducts.innerHTML = count;
 };
 
+
+const renderBrands = brands => {
+  const options = brands
+    .map(brand => `<option value="${brand}">${brand}</option>`)
+    .join('');
+
+  brandSelect.innerHTML = options;
+};
+
+const renderBrandsCount = () => {
+  const spanNbBrands = document.querySelector('#nbBrands');
+  spanNbBrands.innerHTML = brandsCount;
+};
+
+const renderRecentProducts = () => {
+  const spanNbRecentProducts = document.querySelector('#nbRecentProducts');
+  spanNbRecentProducts.innerHTML = recentProducts;
+};
+
+const renderLastRelease = () => {
+  const spanLastReleased = document.querySelector('#spanLastReleased');
+  spanLastReleased.innerHTML = lastRelease;
+};
+
+const renderStats = () => {
+  const spanP50 = document.querySelector('#spanP50');
+  spanP50.innerHTML = p50;
+  const spanP90 = document.querySelector('#spanP90');
+  spanP90.innerHTML = p90;
+  const spanP95 = document.querySelector('#spanP95');
+  spanP95.innerHTML = p95;
+};
+
 const render = (products, pagination) => {
   renderProducts(products);
   renderPagination(pagination);
   renderIndicators(pagination);
+  renderBrandsCount();
+  renderRecentProducts();
+  renderLastRelease();
+  renderStats();
 };
 
-/**
- * Declaration of all Listeners
- */
+async function fetchBrands() {
+  try {
+    const response = await fetch(
+      'https://clear-fashion-api.vercel.app/brands'
+    );
+    const body = await response.json();
 
-/**
- * Select the number of products to display
- */
+    if (body.success !== true) {
+      console.error(error);
+    }
+    else {
+      var brands = body.data.result;
+      brands.unshift("all");
+      renderBrands(brands);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function addToFavorite(product) {
+  var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  favorites.push(product);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  document.getElementById(product + "-fav").innerHTML = `🖤 <button onclick=deleteToFavorite("` + product + `")>Remove</button>`;
+}
+
+async function deleteToFavorite(product) {
+  var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  favorites = favorites.filter(favorite => favorite != product);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  document.getElementById(product + "-fav").innerHTML = `<button onclick=addToFavorite("` + product + `")>Favorite</button>`;
+  if(showOnlySelectFavorite.checked){
+    const products = await fetchProducts(1, currentPagination.pageSize, brandSelect.value, sortSelect.value, [showOnlySelectSale.checked, showOnlySelectNew.checked, showOnlySelectFavorite.checked]);
+    setCurrentProducts(products);
+    render(currentProducts, currentPagination);
+  }
+}
+
 selectShow.addEventListener('change', async (event) => {
-  const products = await fetchProducts(currentPagination.currentPage, parseInt(event.target.value));
-  
+  const products = await fetchProducts(1, parseInt(event.target.value), brandSelect.value, sortSelect.value, [showOnlySelectSale.checked, showOnlySelectNew.checked, showOnlySelectFavorite.checked]);
+
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
 
-/**
- * Select the page to display
- */
 selectPage.addEventListener('change', async (event) => {
-  const products = await fetchProducts(parseInt(event.target.value), currentPagination.pageSize);
+  const products = await fetchProducts(parseInt(event.target.value), currentPagination.pageSize, brandSelect.value, sortSelect.value, [showOnlySelectSale.checked, showOnlySelectNew.checked, showOnlySelectFavorite.checked]);
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
 
-/**
- * Select the brand to display
- */
-selectBrand.addEventListener('change', async (event) => {
-  setCurrentBrand(String(event.target.value));
-
-  const products = await fetchProducts(currentPagination.currentPage, currentPagination.pageSize);
+brandSelect.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1, currentPagination.pageSize, event.target.value, sortSelect.value, [showOnlySelectSale.checked, showOnlySelectNew.checked, showOnlySelectFavorite.checked]);
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
+
+sortSelect.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1, currentPagination.pageSize, brandSelect.value, event.target.value, [showOnlySelectSale.checked, showOnlySelectNew.checked, showOnlySelectFavorite.checked]);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+showOnlySelectFavorite.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1, currentPagination.pageSize, brandSelect.value, sortSelect.value, [showOnlySelectSale.checked, showOnlySelectNew.checked, event.target.checked]);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+showOnlySelectSale.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1, currentPagination.pageSize, brandSelect.value, sortSelect.value, [event.target.checked, showOnlySelectNew.checked, showOnlySelectFavorite.checked]);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+showOnlySelectNew.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1, currentPagination.pageSize, brandSelect.value, sortSelect.value, [showOnlySelectSale.checked, event.target.checked, showOnlySelectFavorite.checked]);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
 
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -211,8 +326,4 @@ document.addEventListener('DOMContentLoaded', async () => {
   render(currentProducts, currentPagination);
 });
 
-selectSortingType.addEventListener('change', async (event) => {
-  console.log(String(event.target.value));
-  sortProducts(String(event.target.value));
-  render(currentProducts, currentPagination);
-})
+fetchBrands();
